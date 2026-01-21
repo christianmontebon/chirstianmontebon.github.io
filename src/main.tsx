972c250e4e318999438eec081df3863bfa8845af
cm <cpgmontebon@gmail.com>
2026-01-22 02:52:09 +0800

    use HashRouter instead to fix the issue on routes in github pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,20 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'highlight.js/styles/github-dark-dimmed.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import ProjectsPage from './pages/ProjectsPage'
 import NotesPage from './pages/NotesPage'
 import MarkdownRenderer from './components/MarkdownRenderer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/notes" element={<NotesPage />} />
         <Route path="/notes/:slug" element={<MarkdownRenderer />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
